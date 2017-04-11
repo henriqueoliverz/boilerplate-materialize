a6f3e3541d8818bfb4db3ddb861c7e56d1e5d447
Henrique Oliveira <henrique.oliveira@am4.com.br>
2017-04-11 20:12:38 -0300

    Segmentando as partes do projeto

---
+++ AdminMaterialize/Controllers/BaseController.cs
@@ -0,0 +1,21 @@
+﻿using AdminMaterialize.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdminMaterialize.Controllers
+{
+    public class BaseController : Controller
+    {
+        public List<Breadcrumb> Breadcrumbs { get; set; }
+        public string Titulo { get; set; }
+
+        public BaseController()
+        {
+            ViewBag.Breadcrumbs = Breadcrumbs;
+            ViewBag.Titulo = Titulo;
+        }
+    }
+}
