8606cc331eb9acc73cc867dfc612a7d95036acab
Henrique Oliveira <henrique.oliveira@am4.com.br>
2017-04-12 00:51:23 -0300

    Ultimos ajustes

--- AdminMaterialize/Controllers/AccountController.cs
+++ AdminMaterialize/Controllers/AccountController.cs
@@ -13,5 +13,10 @@ namespace AdminMaterialize.Controllers
         {
             return View();
         }
+
+        public ActionResult LogOff()
+        {
+            return RedirectToAction("Index");
+        }
     }
 }
